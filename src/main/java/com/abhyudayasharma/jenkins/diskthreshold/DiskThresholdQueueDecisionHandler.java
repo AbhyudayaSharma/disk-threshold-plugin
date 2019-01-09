a93967285b27b1f26f7fcb0325ee6bb3a93c1366
Abhyudaya Sharma <sharmaabhyudaya@gmail.com>
2019-01-09 13:45:59 +0530

    Remove unnecessary instanceof checks

--- src/main/java/com/abhyudayasharma/jenkins/diskthreshold/DiskThresholdQueueDecisionHandler.java
+++ src/main/java/com/abhyudayasharma/jenkins/diskthreshold/DiskThresholdQueueDecisionHandler.java
@@ -1,14 +1,13 @@
 package com.abhyudayasharma.jenkins.diskthreshold;
 
 import hudson.Extension;
+import hudson.model.AbstractProject;
 import hudson.model.Action;
 import hudson.model.Job;
 import hudson.model.JobProperty;
-import hudson.model.Project;
 import hudson.model.Queue;
 import jenkins.model.Jenkins;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -19,23 +18,18 @@ public class DiskThresholdQueueDecisionHandler extends Queue.QueueDecisionHandle
 
     @Override
     public boolean shouldSchedule(Queue.Task p, List<Action> actions) {
-        if (p instanceof Project) {
-            Project project = (Project) p;
-            Collection jobs = project.getAllJobs();
-            for (Object o : jobs) {
-                if (o instanceof Job) {
-                    Job job = (Job) o;
-                    JobProperty prop = job.getProperty(DiskThresholdJobProperty.class);
-                    if (prop instanceof DiskThresholdJobProperty) { // instanceof checks for null
-                        long usableSpace = Jenkins.getInstance().getRootDir().getUsableSpace();
-                        DiskThresholdJobProperty property = (DiskThresholdJobProperty) prop;
-                        long thresholdBytes = property.getThresholdMegaBytes() * 1024 * 1024; // convert to bytes from MBs
-                        if (usableSpace < thresholdBytes) {
-                            LOGGER.log(Level.SEVERE, "Not adding task to Queue due to limited disk space. " +
-                                    "Current free disk space: " + usableSpace);
-                            return false;
-                        }
-                    }
+        AbstractProject project = (AbstractProject) p;
+        for (Object o : project.getAllJobs()) {
+            Job job = (Job) o;
+            JobProperty prop = job.getProperty(DiskThresholdJobProperty.class);
+            if (prop instanceof DiskThresholdJobProperty) { // instanceof checks for null
+                DiskThresholdJobProperty property = (DiskThresholdJobProperty) prop;
+                long usableSpace = Jenkins.getInstance().getRootDir().getUsableSpace();
+                long thresholdBytes = property.getThresholdMegaBytes() * 1024 * 1024; // convert to bytes from MBs
+                if (usableSpace < thresholdBytes) {
+                    LOGGER.log(Level.SEVERE, "Not adding task to Queue due to limited disk space. " +
+                            "Current free disk space: " + usableSpace);
+                    return false;
                 }
             }
         }
